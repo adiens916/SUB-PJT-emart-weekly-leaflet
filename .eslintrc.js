04741e97f0a5637d0830df818dd71d007360f84b
Inho <adiens916@naver.com>
2022-08-29 09:18:08 +0900

    chore: Fix ES Lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
   rules: {},
 };
