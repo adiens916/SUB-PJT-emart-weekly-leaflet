fed7a5ab73bbc3ce7031008a544a57c3142c3d56
Inho <adiens916@naver.com>
2022-08-30 22:08:00 +0900

    fix: Fix infinite loading
    If fully loaded, then remove div from rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Container } from '@mui/material';
 
 import Intro from './components/Intro';
@@ -7,37 +7,33 @@ import ItemFilter from './components/ItemFilter';
 import ItemList from './components/ItemList';
 
 import useIntersect from './hook/useIntersectionObserver';
-import { getItemListByPage } from './api';
+import { ItemListLoader } from './api';
 import { ItemType } from './types';
 
 function App() {
   const [categoryId, setCategoryId] = useState(0);
   const [itemList, setItemList] = useState<ItemType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isFullLoaded, setIsFullLoaded] = useState(false);
+  const itemListLoader = useRef(new ItemListLoader());
   const categoryIdRef = useRef<number>(0);
-  const pageCount = useRef<number>(1);
 
   const { setRef } = useIntersect(async (entry, observer) => {
-    setIsLoading(true);
     observer.unobserve(entry.target);
+    setIsLoading(true);
 
-    const newItemList = await getItemListByPage(
-      categoryIdRef.current,
-      pageCount.current,
-    );
+    const newItemList = await itemListLoader.current.getMore();
+    console.log(newItemList);
 
-    setItemList((itemList) => [...itemList, ...newItemList]);
-    pageCount.current += 1;
+    if (newItemList.length) {
+      setItemList((itemList) => [...itemList, ...newItemList]);
+    } else {
+      setIsFullLoaded(true);
+    }
 
     setIsLoading(false);
     observer.observe(entry.target);
   }, {});
-
-  useEffect(() => {
-    console.log(categoryIdRef);
-    pageCount.current = 1;
-    setItemList([]);
-  }, [categoryIdRef.current]);
 
   return (
     <Container>
@@ -51,7 +47,11 @@ function App() {
       />
       <ItemList itemList={itemList} />
       {isLoading && <div>Loading...</div>}
-      {!isLoading && <div ref={setRef}></div>}
+      {!isLoading && !isFullLoaded && (
+        <div ref={setRef}>
+          <br />
+        </div>
+      )}
     </Container>
   );
 }
