d35338f08c8e4df1a88a164fabf4cca5b3076853
Inho <adiens916@naver.com>
2022-09-07 09:16:10 +0900

    fix: Change way to load items by using hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,11 @@ import Header from './components/Header';
 import ItemFilter from './components/ItemFilter';
 import ItemList from './components/ItemList';
 
-import useIntersect from './hook/useIntersectionObserver';
 import useItemLoader from './hook/useItemLoader';
-import { ItemListLoader } from './api';
-import { ItemType } from './types';
 
 function App() {
   const [categoryId, setCategoryId] = useState(0);
-  const [itemList, setItemList] = useState<ItemType[]>([]);
-  const [isFullLoaded, setIsFullLoaded] = useState(false);
-  const itemListLoader = useRef(new ItemListLoader());
   const categoryIdRef = useRef<number>(0);
-
-  const { setRef } = useIntersect(async (entry, observer) => {
-    observer.unobserve(entry.target);
-    await loadItemList();
-    observer.observe(entry.target);
-  }, {});
-
-  const loadItemList = async () => {
-    const newItemList = await itemListLoader.current.getMore();
-    console.log(newItemList);
-
-    if (newItemList.length) {
-      setItemList((itemList) => [...itemList, ...newItemList]);
-    } else {
-      setIsFullLoaded(true);
-    }
-  };
 
   const [page, setPage] = useState(1);
   const { items, loading, hasMore } = useItemLoader(page, 0);
@@ -42,13 +19,6 @@ function App() {
     console.log('loading: ', loading);
     console.log('hasMore: ', hasMore);
   }, [items, loading, hasMore]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     itemListLoader.current.setCategory(categoryId);
-  //     setItemList(await itemListLoader.current.getMore());
-  //   })();
-  // }, [categoryId]);
 
   return (
     <Container>
@@ -66,13 +36,8 @@ function App() {
         categoryId={categoryId}
         categoryRef={categoryIdRef}
       />
-      <ItemList itemList={itemList} />
-      {/* {isLoading && <div>Loading...</div>} */}
-      {!isFullLoaded && (
-        <div ref={setRef}>
-          <br />
-        </div>
-      )}
+      <ItemList itemList={items} />
+      {loading && <div>Loading...</div>}
     </Container>
   );
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -12,60 +12,6 @@ export async function fetchItems(
   return await request(`${host}/${resource}?${query}`);
 }
 
-export class ItemListLoader {
-  private categoryId;
-  private limit;
-  private isLoadingFinished;
-  private itemListOriginal: ItemType[] = [];
-  private itemListFiltered: ItemType[] = [];
-
-  constructor() {
-    this.categoryId = 0;
-    this.limit = 10;
-    this.isLoadingFinished = false;
-  }
-
-  setCategory(categoryId: number) {
-    this.categoryId = categoryId;
-    this.isLoadingFinished = false;
-    this.itemListFiltered = [];
-  }
-
-  async getMore() {
-    if (this.isLoadingFinished) {
-      return [];
-    }
-
-    if (this.itemListFiltered.length === 0) {
-      this.itemListFiltered = await this.getFilteredList();
-    }
-
-    const itemList = this.itemListFiltered.splice(0, this.limit);
-    if (this.itemListFiltered.length === 0) {
-      this.isLoadingFinished = true;
-    }
-    return itemList;
-  }
-
-  private async getFilteredList() {
-    await this.loadItem();
-
-    if (!this.categoryId) {
-      return this.itemListOriginal;
-    } else {
-      return this.itemListOriginal.filter((item) =>
-        item.categories.includes(this.categoryId),
-      );
-    }
-  }
-
-  private async loadItem() {
-    if (this.itemListOriginal.length === 0) {
-      this.itemListOriginal = await request<ItemType[]>(host);
-    }
-  }
-}
-
 async function request<T>(url: string): Promise<T> {
   try {
     const response = await fetch(url);
