3e4748186ecddfb78d0ea8146bb762615809a70f
Inho <adiens916@naver.com>
2022-09-07 13:56:44 +0900

    refactor: Use custom hook for Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { Container } from '@mui/material';
 
 import Intro from './components/Intro';
@@ -7,6 +7,7 @@ import ItemFilter from './components/ItemFilter';
 import ItemList from './components/ItemList';
 
 import useItemLoader from './hook/useItemLoader';
+import useObserver from './hook/useObserver';
 
 function App() {
   const [page, setPage] = useState(1);
@@ -18,30 +19,16 @@ function App() {
     setPage(1);
   };
 
-  const observer = useRef<IntersectionObserver>();
-  const onRefAttachObserver = (node: HTMLDivElement) => {
-    // 최초 마운트 시에는 로딩 상태이므로 관측 대상 설정하지 않음
-    if (loading) return;
-    // 이전에 관찰하던 대상들 관찰 해제
-    if (observer.current) observer.current.disconnect();
-    // 관찰자를 새로 만들어줘야 함 (새로 안 만들면 이전 행동 반복함)
-    observer.current = new IntersectionObserver(onIntersect);
-    // 새로 렌더링된 DOM을 관측 대상으로 설정
-    if (node) observer.current.observe(node);
-  };
+  // 관찰자 생성
+  const { onRefAttachObserver } = useObserver(onIntersect, loading);
 
-  const onIntersect = ([entry]: IntersectionObserverEntry[]) => {
+  // 관측 시 행동
+  function onIntersect([entry]: IntersectionObserverEntry[]) {
     // 이전 API 요청 결과가 더 있었을 때만, 페이지 개수 변경
     if (entry.isIntersecting && hasMore) {
       setPage((page) => page + 1);
     }
-  };
-
-  useEffect(() => {
-    console.log('items: ', items);
-    console.log('loading: ', loading);
-    console.log('hasMore: ', hasMore);
-  }, [items, loading, hasMore]);
+  }
 
   return (
     <Container>
